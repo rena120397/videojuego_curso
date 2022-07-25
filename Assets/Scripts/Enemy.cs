ba5e131f24f6bdf5ea44c9f43a68768e75408073
Ronald Chevarria <renato120397@gmail.com>
2022-07-25 14:54:43 -0500

    commit para publicar juego en git

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float movHor = 0f;
+    public float speed = 3f;
+
+    public bool isGroundFloor = true;
+    public bool isGroundFront = false;
+
+    public LayerMask groundLayer;
+    public float frontGrndRayDist = 0.25f;
+    public float floorCheckY = 0.52f;
+    public float frontCheck = 0.51f;
+    public float frontDist = 0.001f;
+
+    public int scoreGive = 50;
+
+    private RaycastHit2D hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Evitar caer en precipicio
+        isGroundFloor = (Physics2D.Raycast(new Vector3(transform.position.x,
+                            transform.position.y - floorCheckY,
+                            transform.position.z),
+            new Vector3(movHor, 0, 0), frontGrndRayDist, groundLayer));
+
+        if (!isGroundFloor) //Mientras encuentre piso no haga nada hasta que  deje que  encontrar piso  y haga el cambio
+            movHor = movHor * -1;
+
+        //Choque con pared
+        if (Physics2D.Raycast(transform.position,
+            new Vector3(movHor, 0, 0), frontCheck, groundLayer))
+            movHor = movHor * -1;
+
+        //Choque con otro enemigo
+        hit = Physics2D.Raycast(new Vector3(transform.position.x + movHor * frontCheck,
+            transform.position.y, transform.position.z),
+            new Vector3(movHor, 0, 0), frontDist);
+
+        if (hit != null)
+            if (hit.transform != null)
+                if (hit.transform.CompareTag("Enemy"))
+                    movHor = movHor * -1;
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movHor * speed, rb.velocity.y);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Dañar Player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Dañar Player
+            Debug.Log("Daño al personaje");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Destruir enemigo
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Destruir enemigo
+            getKilled();
+        }
+    }
+    private void getKilled() //función para cuando nuestro enemigo es eliminado
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player obj;
+    public int lives = 3;
+    public bool isGrounded = false;
+    public bool isMoving = false;
+    public bool isImmune = false;
+
+    public float speed = 5;
+    public float jumpForce = 3f;
+    public float movHor;
+
+    public float immuneTimeCnt = 0f;
+    public float immuneTime = 0.5f;
+
+    public LayerMask groundLayer;
+    public float radius = 0.3f;
+    public float groundRayDist = 0.5f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        obj = this;
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movHor = Input.GetAxisRaw("Horizontal");
+        isMoving = (movHor != 0f); //actualizando variable isMoving
+        isGrounded = Physics2D.CircleCast(transform.position, radius, Vector3.down, groundRayDist, groundLayer); //Determina si el personaje está tocando el piso.
+
+        if (Input.GetKeyDown(KeyCode.Space))//barra esoaciadora para saltar       
+            jump();
+
+        flip(movHor);
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movHor * speed, rb.velocity.y);
+    }
+    private void OnDestroy()
+    {
+        obj = null;
+    }
+    public void jump()
+    {
+        if (!isGrounded) return;
+        rb.velocity = Vector2.up * jumpForce;        
+    }
+    private void flip(float _xValue)
+    {
+        Vector3 theScale = transform.localScale;
+        if (_xValue < 0)
+            theScale.x = Mathf.Abs(theScale.x) * -1;
+        else
+            if (_xValue > 0)
+            theScale.x = Mathf.Abs(theScale.x);
+        transform.localScale = theScale;
+    }
+}
